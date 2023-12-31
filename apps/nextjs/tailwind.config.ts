052d1b5a1d99a394be15e96b582f2e94520df866
Hendrik Nieländer <hendrik@nielaender.de>
2023-12-31 16:00:51 +0100

    remove nextjs app

--- apps/nextjs/tailwind.config.ts
+++
@@ -1,8 +0,0 @@
-import type { Config } from "tailwindcss";
-
-import baseConfig from "@acme/tailwind-config";
-
-export default {
-  content: [...baseConfig.content, "../../packages/ui/src/**/*.{ts,tsx}"],
-  presets: [baseConfig],
-} satisfies Config;
